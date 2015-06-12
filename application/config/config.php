ec90170202a3ec2ed7b7afa7a3edc85975164049
albert <albert.szczesny@yahoo.com>
2015-06-12 01:56:01 +0100

    Ignoreing config files

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://rma.iqutech.com';
+$config['base_url'] = 'http://rma.iqutech.dev';
 
 
 /*
